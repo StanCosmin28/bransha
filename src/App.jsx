3d452ff92321348d0559cf63b9e882cf542289d3
Stan Cosmin <stancosminioan@gmail.com>
2025-06-16 15:08:16 +0300

    change order of components

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
   return (
     <>
       {/* <LoadingWrapper timeoutMs={2000}> */}
-      <AppleScrollHero />
       <SimpleBlockComponent />
+      <AppleScrollHero />
       {/* <SimpleBranshaComponent /> */}
       <SmartHomePanel />
       {/* <SimpleShowcaseComponent /> */}
