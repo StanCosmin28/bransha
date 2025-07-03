864391d5db8d3457b4bafb5b759fa57ae9d5f3cb
Stan Cosmin <stancosminioan@gmail.com>
2025-07-03 15:53:46 +0300

    munn page changes

--- src/Components/MUNNCustomLayoutV1.jsx
+++ src/Components/MUNNCustomLayoutV1.jsx
@@ -1,30 +1,50 @@
 export default function MUNNCustomLayoutV1({
-  imageSrc = "https://cdn.thewirecutter.com/wp-content/media/2024/05/protablets-2048px-232431.jpg?auto=webp&quality=75&width=1024",
-  title = "MUNN - Your Digital Building Brain",
-  descriptions = [
-    "athis is a descriptionription for the MUNN application!",
-    "athis is a descriptionription for the MUNN application!",
-    "athis is a descriptionription for the MUNN application!",
-    "athis is a descriptionription for the MUNN application!",
-  ],
+  imageSrc = "",
+  title = "Rapoarte",
+  descriptions = ["Consumuri", "Activitate", "Eficiență"],
   altText = "Section image",
+  description2 = "Totul clar și automat.",
 }) {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center h-auto  text-white p-6 relative">
-      <div className="w-full lg:w-2/3 p-10">
+    <div className="flex flex-col lg:flex-row items-center justify-center h-auto text-white p-6 relative">
+      <div className="w-full lg:w-2/5 p-4 space-y-4 min-h-[400px] flex flex-col justify-between text-left lg:ml-10">
+        <h2 className="text-5xl font-bold text-white">
+          {title}{" "}
+          <span className="bg-green-400 p-2 rounded-md text-black ml-2">
+            Smart
+          </span>
+        </h2>
+        <ul>
+          {descriptions.map((desc, index) => (
+            <li
+              className="flex justify-start items-center p-2 gap-2 text-3xl font-bold"
+              key={index}
+            >
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-4"
+              >
+                <circle cx="9.85156" cy="10.1348" r="9.5" fill="#33E281" />
+              </svg>
+              {desc}
+            </li>
+          ))}
+        </ul>
+        <div className="w-fit relative">
+          <h3 className="text-4xl font-bold text-white mb-3">{description2}</h3>
+          <div className="w-[55%] h-2 bg-green-400 absolute -right-[15%]"></div>
+        </div>
+      </div>
+      <div className="w-full lg:w-3/5 p-4">
         <img
           src={imageSrc}
           alt={altText}
-          className="w-[90%] h-auto object-cover rounded-lg"
+          className="w-[90%] h-auto object-cover rounded-lg mx-auto"
         />
-      </div>
-      <div className="w-full lg:w-1/3 p-4 space-y-4">
-        <h2 className="text-3xl font-bold text-green-400">{title}</h2>
-        <ul>
-          {descriptions.map((desc, index) => (
-            <li key={index}>{desc}</li>
-          ))}
-        </ul>
       </div>
     </div>
   );
--- src/Components/MUNNCustomLayoutV2.jsx
+++ src/Components/MUNNCustomLayoutV2.jsx
@@ -1,7 +1,8 @@
 export default function MUNNCustomLayoutV2({
-  imageSrc = "",
+  imageSrc = "https://cdn.thewirecutter.com/wp-content/media/2024/05/protablets-2048px-232431.jpg?auto=webp&quality=75&width=1024",
   title = "MUNN - Your Digital Building Brain",
   descriptions = [
+    "athis is a descriptionription for the MUNN application!",
     "athis is a descriptionription for the MUNN application!",
     "athis is a descriptionription for the MUNN application!",
     "athis is a descriptionription for the MUNN application!",
@@ -9,7 +10,14 @@ export default function MUNNCustomLayoutV2({
   altText = "Section image",
 }) {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center h-auto text-white p-6 relative">
+    <div className="flex flex-col lg:flex-row items-center justify-center h-auto  text-white p-6 relative">
+      <div className="w-full lg:w-2/3 p-10">
+        <img
+          src={imageSrc}
+          alt={altText}
+          className="w-[90%] h-auto object-cover rounded-lg"
+        />
+      </div>
       <div className="w-full lg:w-1/3 p-4 space-y-4">
         <h2 className="text-3xl font-bold text-green-400">{title}</h2>
         <ul>
@@ -17,13 +25,6 @@ export default function MUNNCustomLayoutV2({
             <li key={index}>{desc}</li>
           ))}
         </ul>
-      </div>
-      <div className="w-full lg:w-2/3 p-4">
-        <img
-          src={imageSrc}
-          alt={altText}
-          className="w-[90%] h-auto object-cover rounded-lg"
-        />
       </div>
     </div>
   );
